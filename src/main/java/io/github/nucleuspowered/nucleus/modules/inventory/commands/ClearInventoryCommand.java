fe6f42b4d13e676ecc55d36157b97bf3533d1a6d
Daniel Naylor <git@drnaylor.co.uk>
2018-09-29 13:18:12 +0100

    Add clear inventory events.

--- src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/ClearInventoryCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/ClearInventoryCommand.java
@@ -13,6 +13,8 @@ import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
 import io.github.nucleuspowered.nucleus.internal.command.NucleusParameters;
 import io.github.nucleuspowered.nucleus.internal.command.ReturnMessageException;
 import io.github.nucleuspowered.nucleus.internal.docgen.annotations.EssentialsEquivalent;
+import io.github.nucleuspowered.nucleus.modules.inventory.events.ClearInventoryEvent;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -32,24 +34,45 @@ public class ClearInventoryCommand extends AbstractCommand<CommandSource> {
     @Override
     public CommandElement[] getArguments() {
         return new CommandElement[] {
-            GenericArguments.optional(
-                GenericArguments.requiringPermission(
-                        NucleusParameters.ONE_USER,
-                        this.permissions.getPermissionWithSuffix("others")
-                ))
+            GenericArguments.flags().flag("a", "-all").buildWith(
+                GenericArguments.optional(
+                        GenericArguments.requiringPermission(
+                                NucleusParameters.ONE_USER,
+                                this.permissions.getPermissionWithSuffix("others")
+                        ))
+            )
         };
     }
 
     @Override protected CommandResult executeCommand(CommandSource source, CommandContext args) throws Exception {
         User user = this.getUserFromArgs(User.class, source, NucleusParameters.Keys.USER, args);
+        boolean all = args.hasAny("a");
         if (user.getPlayer().isPresent()) {
             Player target = user.getPlayer().get();
-            Util.getStandardInventory(target).clear();
+            if (Sponge.getEventManager().post(new ClearInventoryEvent.Pre(Sponge.getCauseStackManager().getCurrentCause(), target, all))) {
+                source.sendMessage(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.clearinventory.cancelled", target.getName()));
+                return CommandResult.empty();
+            }
+            if (all) {
+                target.getInventory().clear();
+            } else {
+                Util.getStandardInventory(target).clear();
+            }
+            Sponge.getEventManager().post(new ClearInventoryEvent.Post(Sponge.getCauseStackManager().getCurrentCause(), target, all));
             source.sendMessage(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.clearinventory.success", target.getName()));
             return CommandResult.success();
         } else {
             try {
-                Util.getStandardInventory(user).clear();
+                if (Sponge.getEventManager().post(new ClearInventoryEvent.Pre(Sponge.getCauseStackManager().getCurrentCause(), user, all))) {
+                    source.sendMessage(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.clearinventory.cancelled", user.getName()));
+                    return CommandResult.empty();
+                }
+                if (all) {
+                    user.getInventory().clear();
+                } else {
+                    Util.getStandardInventory(user).clear();
+                }
+                Sponge.getEventManager().post(new ClearInventoryEvent.Post(Sponge.getCauseStackManager().getCurrentCause(), user, all));
                 source.sendMessage(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.clearinventory.success", user.getName()));
                 return CommandResult.success();
             } catch (UnsupportedOperationException e) {

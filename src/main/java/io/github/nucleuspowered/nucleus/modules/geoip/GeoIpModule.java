65f9524bed11d8664a8cd177d6875692676cc247
Daniel Naylor <git@drnaylor.co.uk>
2018-10-07 14:37:32 +0100

    Add warning for GeoIP module that Nucleus 1.7 will see it split out

--- src/main/java/io/github/nucleuspowered/nucleus/modules/geoip/GeoIpModule.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/geoip/GeoIpModule.java
@@ -6,6 +6,10 @@ package io.github.nucleuspowered.nucleus.modules.geoip;
 
 import io.github.nucleuspowered.nucleus.internal.qsml.module.ConfigurableModule;
 import io.github.nucleuspowered.nucleus.modules.geoip.config.GeoIpConfigAdapter;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 import uk.co.drnaylor.quickstart.annotations.ModuleData;
 import uk.co.drnaylor.quickstart.enums.LoadingStatus;
 
@@ -18,4 +22,12 @@ public class GeoIpModule extends ConfigurableModule<GeoIpConfigAdapter> {
         return new GeoIpConfigAdapter();
     }
 
+    @Override protected void performPreTasks() throws Exception {
+        ConsoleSource source = Sponge.getServer().getConsole();
+        source.sendMessage(Text.of(TextColors.RED, "WARNING: Deprecation of GeoIP module"));
+        source.sendMessage(Text.of(TextColors.RED, "-------------------------------------"));
+        source.sendMessage(Text.of(TextColors.RED, "In Nucleus 1.7, the GeoIP module will be separated out into its own plugin, Heisenberg."));
+        source.sendMessage(Text.of(TextColors.RED, "This will be available on Ore as Nucleus Heisenberg."));
+        super.performPreTasks();
+    }
 }
